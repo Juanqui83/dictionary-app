d082cddcba28f031460d4e1b633459c69dc5b4e4
Juan Carlos Machaca Mamani <juancarlosmachaca@hotmail.com>
2024-09-14 16:42:11 -0400

    10mo commit

--- src/router/router.js
+++ src/router/router.js
@@ -1,16 +1,16 @@
-import { HashRouter as Router, Route, Switch } from 'react-router-dom';
-import DictionaryScreen from './screens/DictionaryScreen';
+import { createBrowserRouter } from "react-router-dom";
+import { App } from "../App";
 
-const basename = process.env.NODE_ENV === 'production' ? '/dictionary-app' : '/';
+const routes = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <App />,
+    },
+  ],
+  {
+    basename: import.meta.env.BASE_URL,
+  }
+);
 
-function AppRouter() {
-  return (
-    <Router basename={basename}>
-      <Switch>
-        <Route path="/dictionary" component={DictionaryScreen} />
-      </Switch>
-    </Router>
-  );
-}
-
-export default AppRouter;
+export default routes;
